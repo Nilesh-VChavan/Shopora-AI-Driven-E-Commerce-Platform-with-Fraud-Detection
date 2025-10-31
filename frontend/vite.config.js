51ab86c2033ad460e213311cf7105b17fdb32ca8
Nilesh Vasant Chavan <nileshvchavan0@gmail.com>
2025-10-31 11:22:08 +0530

    Create Vercel.json + update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,13 @@ export default defineConfig({
     proxy: {
       '/api': 'http://127.0.0.1:8000'
     }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
